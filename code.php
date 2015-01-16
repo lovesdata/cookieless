e21b8181b2d57970b8337edc01f47bba70cffc6a
Benjamin Mangold <benjaminmangold@users.noreply.github.com>
2015-01-16 16:44:10 +1100

    Update code.php

--- code.php
+++ code.php
@@ -1,12 +1,5 @@
 
 <!-- Use this code -->
 
-<script type="text/javascript" src="track/multi/track.js.php"></script>
-<noscript><img src="track/multi/track.gif.php" /></noscript>
-
-
-<!-- OR this code -->
-
-<?php
-include_once (dirname (__FILE__) . '/track/single/index.php');
-?>
+<script type="text/javascript" src="track/track.js.php"></script>
+<noscript><img src="track/track.gif.php" /></noscript>
