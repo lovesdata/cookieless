46a0ecb590a307e76b86088a51cab296e169c153
Benjamin Mangold <benjaminmangold@users.noreply.github.com>
2015-01-16 16:53:22 +1100

    Update config.php

--- track/config.php
+++ track/config.php
@@ -3,10 +3,12 @@
 define ("TRACK_URL", "http://www.company.com/track/");
 define ("ANALYTICS_PROPERTY_ID", "UA-XXXXX-X");
 
-define ("DB_HOST", "localhost");
-define ("DB_USER", "root");
-define ("DB_PASSWORD", "aN3RUoVZyjyoV9M");
-define ("DB_DATABASE", "tracking");
+// Uncomment the following lines if you would like to track into your own database
+
+//define ("DB_HOST", "localhost");
+//define ("DB_USER", "root");
+//define ("DB_PASSWORD", "aN3RUoVZyjyoV9M");
+//define ("DB_DATABASE", "tracking");
 
 define ("TIMEOUT_SECONDS", 5);
 
