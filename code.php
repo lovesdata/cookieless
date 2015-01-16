182e1a15c00e32d2087bd30c86d693e3ff112d9d
Benjamin Mangold <benjaminmangold@users.noreply.github.com>
2015-01-16 16:59:16 +1100

    Update code.php

--- code.php
+++ code.php
@@ -1,6 +1,7 @@
 
 <!-- Use this code -->
-<!-- If you are using this in parallel to Google Analytics, then consider removing line #5 and just use the noscript option -->
+<!-- If you are using this in parallel to Google Analytics, then
+consider removing the script line and just use the noscript option -->
 
 <script type="text/javascript" src="track/track.js.php"></script>
 <noscript><img src="track/track.gif.php" /></noscript>
