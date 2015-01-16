8f513fc0534aea579713fb47961c36082e41f98f
benjaminmangold <benjaminmangold@users.noreply.github.com>
2015-01-16 16:39:49 +1100

    Create code.php

---
+++ code.php
@@ -0,0 +1,12 @@
+
+<!-- Use this code -->
+
+<script type="text/javascript" src="track/multi/track.js.php"></script>
+<noscript><img src="track/multi/track.gif.php" /></noscript>
+
+
+<!-- OR this code -->
+
+<?php
+//include_once (dirname (__FILE__) . '/track/single/index.php');
+?>
